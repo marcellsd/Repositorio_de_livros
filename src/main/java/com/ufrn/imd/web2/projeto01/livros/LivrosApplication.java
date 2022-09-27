3a003a6f39c629871eec5236ed1b8a976da05202
HiltonThallyson <hilton.thallyson@hotmail.com>
2022-09-27 13:42:54 -0300

    First commit

---
+++ src/main/java/com/ufrn/imd/web2/projeto01/livros/LivrosApplication.java
@@ -0,0 +1,13 @@
+package com.ufrn.imd.web2.projeto01.livros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LivrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LivrosApplication.class, args);
+	}
+
+}
