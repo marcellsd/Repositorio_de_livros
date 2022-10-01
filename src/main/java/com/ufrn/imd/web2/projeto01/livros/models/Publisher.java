cbd6b547812dd10c344d7e0fd372b383215861b6
HiltonThallyson <hilton.thallyson@hotmail.com>
2022-10-01 11:34:36 -0300

    fix - correção relação one to one e nome address

--- src/main/java/com/ufrn/imd/web2/projeto01/livros/models/Publisher.java
+++ src/main/java/com/ufrn/imd/web2/projeto01/livros/models/Publisher.java
@@ -8,10 +8,10 @@ import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
-
-import org.hibernate.annotations.CollectionId;
 
 @Entity
 @Table(name = "publishers")
@@ -20,14 +20,14 @@ public class Publisher {
     @Column(length = 50)
     private String name;
 
-    @Column(length = 150)
-    private String hqLocation;
-
-    @Column(length = 150)
-    private String webSite;
+    
 
     @OneToMany(mappedBy = "publisher", fetch = FetchType.LAZY)
     private List<Book> books;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "address_id")
+    private Address address;
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -37,29 +37,17 @@ public class Publisher {
         
     }
 
-    public Publisher(String name, String hqLocation, String webSite) {
+    public Publisher(String name) {
         this.name = name;
-        this.hqLocation = hqLocation;
-        this.webSite = webSite;
     }
+
     public String getName() {
         return name;
     }
     public void setName(String name) {
         this.name = name;
     }
-    public String getHqLocation() {
-        return hqLocation;
-    }
-    public void setHqLocation(String hqLocation) {
-        this.hqLocation = hqLocation;
-    }
-    public String getWebSite() {
-        return webSite;
-    }
-    public void setWebSite(String webSite) {
-        this.webSite = webSite;
-    }
+   
     public Integer getId() {
         return id;
     }
@@ -74,10 +62,6 @@ public class Publisher {
         this.books = books;
     }
 
-    @Override
-    public String toString() {
-        return "Publisher [ name=" + name + ", hqLocation=" + hqLocation + ", id=" + id 
-                + ", webSite=" + webSite + "]";
-    }
+  
     
 }
