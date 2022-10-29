a2ce2bad1981b6de51882f102cc5640e4ffa78cc
HiltonThallyson <hilton.thallyson@hotmail.com>
2022-10-29 18:05:52 -0300

    fix-application properties

--- src/main/java/com/ufrn/imd/web2/projeto01/livros/controllers/BookController.java
+++ src/main/java/com/ufrn/imd/web2/projeto01/livros/controllers/BookController.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpStatus;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
