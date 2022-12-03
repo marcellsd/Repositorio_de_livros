7918f03fb661a46f6aa196225b1b1cb0ffd1a7e5
Marcell Santana de Deus <marcellsantana@gmail.com>
2022-12-03 16:09:19 -0300

    Feat - Add Documentation by Swagger

---
+++ src/main/java/com/ufrn/imd/web2/projeto01/livros/config/SwaggerConfig.java
@@ -0,0 +1,37 @@
+package com.ufrn.imd.web2.projeto01.livros.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI springOpenAPI() {
+        final String securitySchemeName = "bearerAuth";
+        return new OpenAPI()
+                .addSecurityItem(new SecurityRequirement()
+                        .addList(securitySchemeName))
+                .components(new Components()
+                        .addSecuritySchemes(securitySchemeName, new SecurityScheme()
+                                .name(securitySchemeName)
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")))         
+                .info(new Info().title("BookRepository API")
+                        .description("BookRepository sample application")
+                        .version("v0.0.1"))
+                .externalDocs(new ExternalDocumentation()
+                        .description("BookRepository Wiki Documentation")
+                        .url("https://github.com/marcellsd/Repositorio_de_livros"));
+    }  
+
+}
